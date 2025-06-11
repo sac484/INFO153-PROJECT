9612783fa28b912c91fb61a49bd099a6ba60289c
jc4768 <jc4768@drexel.edu>
2025-06-11 15:51:11 -0400

    Update scraper.py with comments

--- scraper.py
+++ scraper.py
@@ -1,15 +1,19 @@
-
+# Import the libraries
 import requests
 from bs4 import BeautifulSoup
 import re
 import time
 
+# Define headers
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
 }
+
+# Create session
 web_session = requests.Session()
 web_session.headers.update(headers)
 
+# Define Queue
 class Queue:
     def __init__(self):
         self.items = []
@@ -26,15 +30,20 @@ class Queue:
     def size(self):
         return len(self.items)
 
+# Get all URL for a specific ZIP code given from Redfin
 def get_all_page_urls(zip_code):
     url_queue = Queue()
     base_url = f"https://www.redfin.com/zipcode/{zip_code}/"
     page_regex = re.compile("Viewing page \\d+ of (\\d+)")
+
+    # Send initial request
     request = web_session.get(base_url)
     if request.status_code != 200:
         return url_queue
     html_body = request.text
     page = BeautifulSoup(html_body, "html.parser")
+
+    # Element that gives number of pages in total
     element = page.find("span", attrs={"data-rf-test-name": "download-and-save-page-number-text"})
     if element is None:
         return url_queue
@@ -47,6 +56,7 @@ def get_all_page_urls(zip_code):
         url_queue.enqueue(f"{base_url}page-{i}")
     return url_queue
 
+# Gets all property listings for a ZIP code
 def get_all_listings(zip_code):
     page_url_queue = get_all_page_urls(zip_code)
     all_listings = []
@@ -55,32 +65,45 @@ def get_all_listings(zip_code):
         html_body = web_session.get(url).text
         page = BeautifulSoup(html_body, "html.parser")
         for listing_card in page.find_all("div", class_="HomeCardContainer"):
+
+            # Skips ads
             if listing_card.get("aria-label") == "Advertisement":
                 continue
             try:
+                # Gets listing details
                 listing_href = listing_card.find("a", href=True, class_="bp-InteractiveHomecard")["href"]
                 price = listing_card.find("span", class_="bp-Homecard__Price--value").text
                 price = float(price[1:].replace(",", ""))
                 address = listing_card.find("div", class_="bp-Homecard__Address").string
+
+                # Number of bedrooms
                 num_beds = listing_card.find("span", class_="bp-Homecard__Stats--beds").string.split(" ")[0]
                 if num_beds == "—":
                     continue
                 else:
                     num_beds = float(num_beds)
+
+                # Number of bathrooms
                 num_baths = listing_card.find("span", class_="bp-Homecard__Stats--baths").string.split(" ")[0]
                 if num_baths == "—":
                     continue
                 else:
                     num_baths = float(num_baths)
+
+                # Square footage
                 square_footage = listing_card.find("span", class_="bp-Homecard__Stats--sqft").find_all(string=True)[0]
                 if square_footage in ["—", "sq ft"]:
                     continue
                 else:
                     square_footage = float(square_footage.replace(",", ""))
+
+                # Contact info for agent
                 try:
                     contact_info = listing_card.find("div", class_="bp-Homecard__Attribution").string
                 except AttributeError:
                     contact_info = "See listing via URL"
+
+                # Dictionary
                 listing_info = {
                     "listing_url": f"https://redfin.com{listing_href}",
                     "price": price,
@@ -96,10 +119,12 @@ def get_all_listings(zip_code):
         time.sleep(1)
     return all_listings
 
+# Apply filters
 def filter_listings(all_listings, min_price=None, max_price=None, min_beds=None, max_beds=None,
                     min_baths=None, max_baths=None, min_sqft=None, max_sqft=None):
     filtered_listings = []
     for listing in all_listings:
+        # Filter conditions for each listing
         conditions = [
             (min_price is None or listing["price"] >= min_price),
             (max_price is None or listing["price"] <= max_price),
